9e223d8e482ab7b5908a58196558682186776b47
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-16 07:38:45 -0700

    fix sonar tests

--- backend/src/main/java/com/ninabornemann/backend/Paper.java
+++
@@ -1,8 +0,0 @@
-package com.ninabornemann.backend;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "paper")
-public record Paper(@Id String id) {
-}
--- backend/src/main/java/com/ninabornemann/backend/Repo/PaperRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ninabornemann.backend.Repo;
-
-import com.ninabornemann.backend.Paper;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaperRepo extends MongoRepository<Paper, String> {
-}
