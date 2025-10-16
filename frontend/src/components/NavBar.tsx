e4d988fb7043a6e9d6fd31ec2e9eadc4f29f26bc
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-16 08:24:02 -0700

    css for navbar and logo

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+export default function NavBar() {
+
+    return (
+        <>
+            <header>
+                <div className={"logo-container"}>
+                    <img src={"src/assets/logo.png"} alt={"Logo"} className={"logo"}/>
+                </div>
+                <nav>
+
+                </nav>
+            </header>
+        </>
+    )
+}
