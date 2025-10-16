9681d28df8636514cd013802a0c6b42c16539189
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-16 02:58:33 -0700

    update .gitignore and add backend module

---
+++ backend/src/test/java/com/ninabornemann/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
