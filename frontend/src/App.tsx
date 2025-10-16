6a927f96c83337ece13b521c23d5ea000c26a422
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-16 08:48:24 -0700

    css footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import NavBar from "./components/NavBar.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
 
   return (
     <>
         <NavBar />
+
+        <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+export default function Footer() {
+
+    return (
+        <>
+            <footer>
+                <p>
+                    © 2025 LitScience — Where AI meets curiosity 🔬
+                </p>
+                <p>
+                    <a href="https://github.com/nina-bornemann/LitScience">View source</a>
+                </p>
+            </footer>
+        </>
+    )
+}
