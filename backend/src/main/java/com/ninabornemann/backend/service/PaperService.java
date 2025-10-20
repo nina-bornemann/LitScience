41103a231a15a5ec566970a639a99f3572964c82
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-20 08:54:02 -0700

    add GetPaperById functions to controller and service
    # Conflicts:
    #	backend/src/main/java/com/ninabornemann/backend/service/PaperService.java

--- backend/src/main/java/com/ninabornemann/backend/service/PaperService.java
+++ backend/src/main/java/com/ninabornemann/backend/service/PaperService.java
@@ -2,7 +2,9 @@ package com.ninabornemann.backend.service;
 import com.ninabornemann.backend.Repo.PaperRepo;
 import com.ninabornemann.backend.model.Paper;
 import com.ninabornemann.backend.model.PaperDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,5 +26,9 @@ public class PaperService {
     public Paper addNewpaper(PaperDto paperDto) {
         Paper newPaper = new Paper(idService.randomId(), paperDto.doi(), paperDto.title(), paperDto.author(), paperDto.year(), paperDto.group(), paperDto.notes());
         return paperRepo.save(newPaper);
+    }
+
+    public Paper getPaperById(String id) {
+        return paperRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No paper was found under this id."));
     }
 }
