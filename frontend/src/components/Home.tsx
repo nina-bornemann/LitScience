6f4eed2ccc1eb4dcc16f61ee95dc52511d331c11
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-22 01:54:43 -0700

    styling of home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -8,15 +8,15 @@ export default function Home() {
                 <h1>🧪    Welcome to LitScience! </h1>
                 <img src={"/src/assets/Smiley_Cell.png"} alt={"Cell-Smiley-Logo"} className={"smiley"}/>
             </div>
-            <div>
+            <div className={"description"}>
                 <h2> ✨ What you can do here: </h2>
                 <p>📚 Build your personal paper collection</p>
                 <p>🔍 Find and import papers online by DOI</p>
                 <p>🧠 Add notes and tags for every paper</p>
                 <p>🗑 Clean up with one-click delete</p>
-                <p>🧾 (Coming soon) Generate AI-powered summaries and reports</p>
-
-                <p> Stay curious, stay organized — and let LitScience take care of the messy part of science reading 💡</p>
+                <p>🧾 Generate AI-powered summaries and reports</p>
+                <br/>
+                <h3> Stay curious, stay organized — and let LitScience take care of the messy part of science reading 💡</h3>
 
             </div>
         </div>
