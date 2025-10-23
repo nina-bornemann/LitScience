7d496a0cc23eaa32d616fbd816269b08e2bf6756
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-23 01:36:04 -0700

    fix table styling when note input is very long

--- frontend/src/components/PaperTable.tsx
+++ frontend/src/components/PaperTable.tsx
@@ -21,12 +21,12 @@ export default function PaperTable(props:Readonly<PaperTableProps>) {
 
     return (
         <>
-            <DataTable onRowClick={navToDetails} rowClassName={() => "clickable"} value={props.papers} footer={footer} dataKey="id" removableSort scrollable scrollHeight="500px" tableStyle={{ minWidth: '40rem'}}>
+            <DataTable onRowClick={navToDetails} rowClassName={() => "clickable"} value={props.papers} footer={footer} dataKey="id" removableSort scrollable scrollHeight="500px" resizableColumns showGridlines={true} tableStyle={{ minWidth: '40rem'}}>
                 <Column field="title" header="Title" sortable style={{ width: '20%' }} className={"truncate-2"}></Column>
-                <Column field="author" header="Author" sortable style={{ width: '15%' }}></Column>
+                <Column field="author" header="Author" sortable style={{ width: '15%' }} className={"truncate-2"}></Column>
                 <Column field="doi" header="DOI" sortable style={{ width: '15%' }} className={"truncate-3"}></Column>
                 <Column field="year" header="Year" sortable style={{ width: '10%' }}></Column>
-                <Column field="group" header="Group" sortable style={{ width: '20%' }}></Column>
+                <Column field="group" header="Group" sortable style={{ width: '20%' }} className={"truncate-2"}></Column>
                 <Column field="notes" header="Notes" sortable style={{ width: '20%' }} className={"truncate-2"}></Column>
             </DataTable>
         </>
