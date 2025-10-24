fcd5d88294300ba05befdc409309ec6856fa55e9
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-24 16:36:53 -0700

    add fav column with hearts if isFav

--- frontend/src/components/PaperTable.tsx
+++ frontend/src/components/PaperTable.tsx
@@ -14,6 +14,14 @@ export default function PaperTable(props:Readonly<PaperTableProps>) {
     const footer = "There are " + props.papers.length + " papers in total. ";
     const nav = useNavigate()
 
+    const favTemplate = (paper:Paper) => {
+        if (paper.isFav) {
+            return <div className={"fav-container"}> <p className={"fav"}> ❤️</p> </div>
+        } else {
+            return <div className={"fav-container"}> <p className={"fav"}>  </p> </div>
+        }
+    };
+
     function navToDetails(event:DataTableRowClickEvent) {
         const paperId = event.data.id
         nav(`/paper/${paperId}`)
@@ -22,6 +30,7 @@ export default function PaperTable(props:Readonly<PaperTableProps>) {
     return (
         <>
             <DataTable onRowClick={navToDetails} rowClassName={() => "clickable"} value={props.papers} footer={footer} dataKey="id" removableSort scrollable scrollHeight="500px" resizableColumns showGridlines={true} tableStyle={{ minWidth: '40rem'}}>
+                <Column field="isFav" header="" sortable style={{ width: '20%' }} body={favTemplate} className={"truncate-2"}></Column>
                 <Column field="title" header="Title" sortable style={{ width: '20%' }} className={"truncate-2"}></Column>
                 <Column field="author" header="Author" sortable style={{ width: '15%' }} className={"truncate-2"}></Column>
                 <Column field="doi" header="DOI" sortable style={{ width: '15%' }} className={"truncate-3"}></Column>
