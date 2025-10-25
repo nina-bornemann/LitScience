9339c665ca57796bb17e8cbf49e67fcf828904f0
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-25 07:31:34 -0700

    first very simple tag frontend system

--- frontend/src/components/GroupSelect.tsx
+++ frontend/src/components/GroupSelect.tsx
@@ -1,19 +1,63 @@
 import "./GroupSelect.css"
-import CreatableSelect from 'react-select/creatable';
+import { TagGroup, Tag, Input, IconButton } from 'rsuite';
+import PlusIcon from '@rsuite/icons/Plus';
+import {useState} from "react";
+import 'rsuite/Tag/styles/index.css'
 
-export default function GroupSelect() {
+export default function GroupSelect(){
+    const [tags, setTags] = useState([]);
+    const [typing, setTyping] = useState(false);
+    const [inputValue, setInputValue] = useState('');
 
-    const options = [
-        { value: 'bio', label: 'Bio' },
-        { value: 'chem', label: 'Chem' },
-        { value: 'physics', label: 'Physics' }
-    ]
+    const removeTag = (tag) => {
+        const nextTags = tags.filter(item => item !== tag);
+        setTags(nextTags);
+    };
 
+    const addTag = () => {
+        const nextTags = inputValue ? [...tags, inputValue] : tags;
+        setTags(nextTags);
+        setTyping(false);
+        setInputValue('');
+    };
+
+    const handleButtonClick = () => {
+        setTyping(true);
+    };
+
+    const renderInput = () => {
+        if (typing) {
+            return (
+                <Input
+                    className="tag-input"
+                    size="xs"
+                    style={{width: 70}}
+                    value={inputValue}
+                    onChange={setInputValue}
+                    onBlur={addTag}
+                    onPressEnter={addTag}
+                />
+            );
+        }
+
+        return (
+            <IconButton
+                className="tag-add-btn"
+                onClick={handleButtonClick}
+                icon={<PlusIcon/>}
+                appearance="ghost"
+                size="xs"
+            />
+        );
+    };
     return (
-        <>
-            <div className={"group-dropdown"}>
-               <CreatableSelect placeholder={"Choose tag"} isMulti options={options}/>
-            </div>
-        </>
-    )
-}
+        <TagGroup>
+            {tags.map((item, index) => (
+                <Tag key={index} closable onClose={() => removeTag(item)}>
+                    {item}
+                </Tag>
+            ))}
+            {renderInput()}
+        </TagGroup>
+    );
+};
