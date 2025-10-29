b7750121dcd8f2ccb230b244edc7b45e4288785d
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-29 15:41:43 -0700

    make dashboard clickable

--- frontend/src/components/DashboardCard.tsx
+++ frontend/src/components/DashboardCard.tsx
@@ -2,13 +2,14 @@ export type DashboardCardProps = {
     emoji:string,
     count:number,
     title:string
+    onClick: () => void
 }
 
 export default function DashboardCard(props:Readonly<DashboardCardProps>) {
 
 
     return (
-        <div className={"stats-card"}>
+        <div className={"stats-card clickable"} onClick={props.onClick}>
             <h1>{props.emoji}</h1>
             <div className={"stats-text"}>
                 <h2>{props.count}</h2>
