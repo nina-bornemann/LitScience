2427d043e1ab0e186ab8ff0b8f67bfbf82c933bb
Nina Bornemann <nina.bornemann@freenet.de>
2025-11-09 09:22:54 -0800

    add SecurityConfig

---
+++ backend/src/main/java/com/ninabornemann/backend/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ninabornemann.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/api/*").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(o -> o.defaultSuccessUrl("https://litscience.onrender.com/"));
+        return http.build();
+    }
+}
